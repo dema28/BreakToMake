0efefcf0afc9a9e20c2a2e821709a18b41e8b8cd
Denis Novicov <dema28ster@gmail.com>
2025-04-08 18:08:49 +0200

    Modify file

---
+++ src/main/java/com/breaktomake/enums/MenuItem.java
@@ -0,0 +1,60 @@
+
+package com.breaktomake.enums;
+
+import com.breaktomake.config.Environment;
+import org.openqa.selenium.By;
+
+public enum MenuItem {
+    KATALOG("Katalog",
+            By.xpath("//h2[text()='Katalog']"), "Katalog",
+            Environment.KATALOG_PAGE),
+
+    PROJEKT("kontakty",
+            By.xpath("//h2[text()='Projekty']"), "Projekty",
+            Environment.PROJEKT_PAGE),
+
+    O_NAS("O nas",
+            By.xpath("//h2[text()='O nás']"), "O nás",
+            Environment.O_NAS_PAGE),
+
+    REALIZACE("Realizace",
+            By.xpath("//h2[text()='Realizace']"), "Realizace",
+            Environment.REALIZACE_PAGE),
+
+    KARIERA("Kariéra",
+            By.xpath("//h2[text()='Kariéra']"),"Kariéra",
+            Environment.KARIERA_PAGE),
+
+    GALERIE("Galerie",
+            By.xpath("//h2[text()='Galerie']"),"Galerie",
+            Environment.GALERIE_PAGE);
+
+    private final String name;
+    private final By locator;
+    private final String expectedHeader;
+    private final String expectedFullUrl;
+
+    MenuItem(String name, By locator, String expectedHeader, String expectedFullUrl) {
+        this.name = name;
+        this.locator = locator;
+        this.expectedHeader = expectedHeader;
+        this.expectedFullUrl = expectedFullUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public By getLocator() {
+        return locator;
+    }
+
+    public String getExpectedHeader() {
+        return expectedHeader;
+    }
+
+    public String getExpectedFullUrl() {
+        return expectedFullUrl;
+    }
+}
+
--- src/main/java/com/breaktomake/pages/MainPage.java
+++ src/main/java/com/breaktomake/pages/MainPage.java
@@ -1,6 +1,7 @@
 package com.breaktomake.pages;
 
 import com.breaktomake.BasePage;
+import com.breaktomake.enums.MenuItem;
 import com.breaktomake.utils.ScrollUtil;
 import io.qameta.allure.Allure;
 import org.openqa.selenium.By;
@@ -15,6 +16,7 @@ import com.breaktomake.config.Environment;
 
 import java.time.Duration;
 import java.util.List;
+
 
 public class MainPage extends BasePage {
 
@@ -48,6 +50,49 @@ public class MainPage extends BasePage {
             return cards.size();
         });
     }
+
+    @Step("Кликаем по пункту меню: {menuItem.name}")
+    public void clickOnMenu(MenuItem menuItem) {
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(Environment.getTimeout()))
+                .until(ExpectedConditions.elementToBeClickable(menuItem.getLocator()));
+        element.click();
+
+        // Задержка после клика
+        try {
+            Thread.sleep(500); // 1.5 секунды
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+    @Step("Проверка заголовка страницы: {expectedHeader} с повторной попыткой")
+    public boolean isHeaderVisible(String expectedHeader) {
+        By headerLocator = By.xpath("//*[contains(@class, 'framer-text') and contains(normalize-space(text()), '" + expectedHeader + "')]");
+        int attempts = 0;
+        while (attempts < 3) {
+            try {
+                WebElement header = new WebDriverWait(driver, Duration.ofSeconds(Environment.getTimeout() + 5))
+                        .until(ExpectedConditions.visibilityOfElementLocated(headerLocator));
+                return header.isDisplayed();
+            } catch (Exception e) {
+                attempts++;
+                try {
+                    Thread.sleep(500); // подождать перед следующей попыткой
+                } catch (InterruptedException ignored) {}
+            }
+        }
+        return false;
+    }
+
+
+    @Step("Получение текущего URL")
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+
+
 
 
 
