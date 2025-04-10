7b5b9c34bc376981644e0c0fd62b7afe4c0a6ef4
Denis Novicov <dema28ster@gmail.com>
2025-04-10 19:44:32 +0200

    Modify LoggerUtil

--- src/main/java/com/breaktomake/utils/LoggerUtil.java
+++ src/main/java/com/breaktomake/utils/LoggerUtil.java
@@ -10,31 +10,56 @@ public class LoggerUtil {
         // Utility class
     }
 
+    // INFO
     public static void info(String message) {
         log.info(message);
     }
 
-    public static void error(String message, Throwable t) {
-        log.error(message, t);
+    public static void info(com.breaktomake.utils.LoggerTag tag, String message) {
+        log.info(tag.getPrefix() + " " + message);
     }
 
-    public static void warn(String message) {
-        log.warn(message);
-    }
-
+    // DEBUG
     public static void debug(String message) {
         log.debug(message);
     }
 
+    public static void debug(com.breaktomake.utils.LoggerTag tag, String message) {
+        log.debug(tag.getPrefix() + " " + message);
+    }
+
+    // WARN
+    public static void warn(String message) {
+        log.warn(message);
+    }
+
+    public static void warn(com.breaktomake.utils.LoggerTag tag, String message) {
+        log.warn(tag.getPrefix() + " " + message);
+    }
+
+    // ERROR
+    public static void error(String message, Throwable t) {
+        log.error(message, t);
+    }
+
+    public static void error(com.breaktomake.utils.LoggerTag tag, String message) {
+        log.error(tag.getPrefix() + " " + message);
+    }
+
+    public static void error(com.breaktomake.utils.LoggerTag tag, String message, Throwable t) {
+        log.error(tag.getPrefix() + " " + message, t);
+    }
+
+    // START/END
     public static void logStart(String testName) {
-        log.info("===== START TEST: " + testName + " =====");
+        log.info(com.breaktomake.utils.LoggerTag.TEST.getPrefix() + " ==========[ 🚀 START TEST: " + testName + " ]==========");
     }
 
     public static void logEnd(String testName) {
-        log.info("===== END TEST: " + testName + " =====");
+        log.info(com.breaktomake.utils.LoggerTag.TEST.getPrefix() + " ==========[ 🧪 END TEST: " + testName + " ]==========");
     }
 
     public static void logError(String message, Throwable t) {
-        log.error("===== ERROR: " + message + " =====", t);
+        log.error(com.breaktomake.utils.LoggerTag.TEST.getPrefix() + " ===== ERROR: " + message + " =====", t);
     }
 }
